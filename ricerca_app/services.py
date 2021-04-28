e1ef427260a9b2096cbf2aa4ede5ee9c0f9d72b9
simonemungarirevelis <77100231+simonemungarirevelis@users.noreply.github.com>
2021-04-28 15:43:36 +0200

    Migrations

--- ricerca_app/services.py
+++ ricerca_app/services.py
@@ -363,6 +363,7 @@ class ServiceDocente:
 
         query = list(query)
 
+
         departments = DidatticaDipartimento.objects.filter(
             dip_cod__in=dip_cods).values(
             "dip_id", "dip_cod", "dip_des_it", "dip_des_eng")
