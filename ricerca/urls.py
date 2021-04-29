2e3f6ff911aa24928217f20072ed613bccf1032d
peppelinux <giuseppe.demarco@unical.it>
2021-04-29 10:22:17 +0200

    uwsgi_setup

--- ricerca/urls.py
+++ ricerca/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{settings.ADMIN_PATH}/', admin.site.urls),
 ]
 
 if 'ricerca_app' in settings.INSTALLED_APPS:
