3c3099e6250937c2e63e22f8b986f9a323501229
Simone Mungari <simone.mungari@revelis.eu>
2021-05-11 15:17:09 +0200

    Added total pages in responses

--- ricerca_app/pagination.py
+++ ricerca_app/pagination.py
@@ -1,7 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class UnicalStorageApiPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 250
+
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data
+        })
