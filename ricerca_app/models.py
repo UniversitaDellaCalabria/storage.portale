0069769aab66470c942bc5b9339d817285f8a0f5
Simone Mungari <simone.mungari@revelis.eu>
2021-06-08 10:50:20 +0200

    Updated models.py

--- ricerca_app/models.py
+++ ricerca_app/models.py
@@ -1616,6 +1616,27 @@ class Personale(InsModAbstract):
         return '{} {} [{}]'.format(self.nome,
                                    self.cognome, self.matricola)
 
+class PersonaleContatti(models.Model):
+    id_ab = models.IntegerField(db_column='ID_AB', primary_key=True)  # Field name made lowercase.
+    cod_fis = models.CharField(db_column='COD_FIS', max_length=16, blank=True, null=True)  # Field name made lowercase.
+    cd_tipo_cont = models.ForeignKey('PersonaleTipoContatto', models.DO_NOTHING, db_column='CD_TIPO_CONT')  # Field name made lowercase.
+    contatto = models.CharField(db_column='CONTATTO', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    prg_priorita = models.IntegerField(db_column='PRG_PRIORITA')  # Field name made lowercase.
+    dt_ins_mod = models.DateField(db_column='DT_INS_MOD', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'PERSONALE_CONTATTI'
+        unique_together = (('id_ab', 'cd_tipo_cont', 'prg_priorita'),)
+
+
+class PersonaleTipoContatto(models.Model):
+    cod_contatto = models.CharField(db_column='COD_CONTATTO', primary_key=True, max_length=20)  # Field name made lowercase.
+    descr_contatto = models.CharField(db_column='DESCR_CONTATTO', max_length=200)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'PERSONALE_TIPO_CONTATTO'
 
 class RicercaAster1(InsModAbstract):
 
