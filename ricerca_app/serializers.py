23893eca4faae10029b79750541dedc0f4b76669
Francesco Filicetti <francesco.filicetti@unical.it>
2023-04-27 18:45:05 +0200

    fix: bug

--- ricerca_app/serializers.py
+++ ricerca_app/serializers.py
@@ -1270,7 +1270,7 @@ class PersonaleSerializer(CreateUpdateAbstract):
     def to_dict(query,
                 req_lang='en'):
         middle_name = f" {query['middle_name']} " if query['middle_name'] else " "
-        full_name = f"{query['name']}{query['middle_name']}{query['cognome']}"
+        full_name = f"{query['nome']}{query['middle_name']}{query['cognome']}"
         functions = None
         if query["Functions"] is not None:
             functions = PersonaleSerializer.to_dict_functions(
