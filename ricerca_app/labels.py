aa76fce8d4a678bb55703b9ffb8772544eee0e99
Francesco Filicetti <francesco.filicetti@unical.it>
2023-06-28 11:37:52 +0200

    fix: labels

--- ricerca_app/labels.py
+++ ricerca_app/labels.py
@@ -488,6 +488,7 @@ LABEL_MAPPING = {
         'StudyManifesto': 'Study manifesto',
         'DidacticRegulation': 'Didactic regulation',
         'TeachingSystem': 'Teaching system',
+        'TeachingSystemYear': 'Teaching system year',
     },
     'it': {
         'RegDidId': 'ID Regolamento Didattico',
@@ -978,5 +979,6 @@ LABEL_MAPPING = {
         'StudyManifesto': 'Manifesto degli studi',
         'DidacticRegulation': 'Regolamento didattico',
         'TeachingSystem': 'Ordinamento didattico',
+        'TeachingSystemYear': 'Anno ordinamento didattico',
     }
 }
