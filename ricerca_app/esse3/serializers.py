d869ff3dedfe7871049fa3f1ef7b81c294654be1
Francesco Filicetti <francesco.filicetti@unical.it>
2023-12-14 11:12:22 +0100

    fix: esse3 appelli serializer

--- ricerca_app/esse3/serializers.py
+++ ricerca_app/esse3/serializers.py
@@ -2,12 +2,14 @@ def esse3AppelliSerializer(appelli, year=None, af_id=None, search={}): # pragma:
     appelli_list = []
     for appello in appelli:
         data = appello['dataInizioApp'].split(" ")[0].split("/")
-        new_data = data[2] + "-" + data[1] + "-" + data[0]
+        new_data = f"{data[2]}-{data[1]}-{data[0]}"
+        ora = appello['oraEsa'].split(" ")[1].split(":")
+        new_ora = f"{ora[0]}:{ora[1]}"
         appello_dict = {
             "insegnamento": appello['adDes'],
             "dataInizio": new_data,
             "dataFine": None,
-            "orarioInizio": appello['oraEsa'].split(" ")[1],
+            "orarioInizio": new_ora,
             "orarioFine": None,
             "annoCorso": None,
             "cfu": None,
