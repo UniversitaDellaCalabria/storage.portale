7de55e37f42ae2476bc36193a760912f95fabf3f
Francesco Filicetti <francesco.filicetti@unical.it>
2024-06-13 09:41:57 +0200

    fix: synthax

--- ricerca_app/api_views.py
+++ ricerca_app/api_views.py
@@ -1349,9 +1349,9 @@ class ApiPhdActivitiesList(ApiEndpointList):
         cycle = request.query_params.get('cycle')
         ssd = request.query_params.get('ssd')
         teacher = request.query_params.get('teacher')
-        tipology = request.query_params.get('tipology')
+        typology = request.query_params.get('typology')
 
-        return ServiceDottorato.getPhdActivities(search, structure, phd, ssd, teacher, cycle, tipology)
+        return ServiceDottorato.getPhdActivities(search, structure, phd, ssd, teacher, cycle, typology)
 
 
 class ApiPhdActivityDetail(ApiEndpointDetail):
