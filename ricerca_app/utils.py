edc34c15cbd4f704149e50e5f7c049231f433c26
Francesco Filicetti <francesco.filicetti@unical.it>
2024-06-21 11:39:38 +0200

    fix: bug

--- ricerca_app/utils.py
+++ ricerca_app/utils.py
@@ -126,7 +126,7 @@ def append_email_addresses(addressbook_queryset, id_ab_key):
     cached_contacts = cache.get(cache_key, [])
     for q in addressbook_queryset:
         emails = []
-        uc = cached_contacts.filter(id_ab=q['id_ab'])
+        uc = cached_contacts.filter(id_ab=q[id_ab_key])
         for contact in uc:
             if any(x in contact['contatto'].lower() for x in PERSON_CONTACTS_EXCLUDE_STRINGS):
                 continue
