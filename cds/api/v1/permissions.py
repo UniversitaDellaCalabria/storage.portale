d9866a6edf6db60579e2d2e3b14f66734763a187
Francesco Filicetti <francesco.filicetti@unical.it>
2025-02-07 13:59:05 +0100

    fix: missing file

---
+++ cds/api/v1/permissions.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import BasePermission
+
+from organizational_area.models import OrganizationalStructureOfficeEmployee
+
+from generics.settings import CURRENT_YEAR
+
+from ...settings import OFFICE_CDS, OFFICE_CDS_DOCUMENTS, OFFICE_CDS_TEACHING_SYSTEM
+
+
+class CdsVisibilityPermission(BasePermission):
+    def has_permission(self, request, view):
+        academic_year = query_params.get("academicyear")
+        if academic_year and academic_year > CURRENT_YEAR:
+            if not request.user.is_authenticated: return False
+            if request.user.is_superuser: return True
+            belongs_to_office = OrganizationalStructureOfficeEmployee.objects.filter(
+                employee=request.user,
+                office__name__in=[
+                    OFFICE_CDS,
+                    OFFICE_CDS_DOCUMENTS,
+                    OFFICE_CDS_TEACHING_SYSTEM
+                ],
+                office__is_active=True,
+                office__organizational_structure__is_active=True,
+            ).exists()
+            return belongs_to_office
+        return True
