3f25c4cac8a93adb03d8edfacfea26093c951b63
revelis-martina <martina.imbrogno@revelis.eu>
2025-04-09 18:50:08 +0200

    feat: research_lines added url, seriliazer, view and filter for AppliedResearchLines

--- research_lines/api/v2/serializers.py
+++ research_lines/api/v2/serializers.py
@@ -6,7 +6,12 @@ from generics.utils import encrypt
 #     extend_schema_field,
 #     extend_schema_serializer,
 # )
-from research_groups.models import RicercaGruppo
+from research_lines.models import (
+    RicercaDocenteLineaApplicata,
+    RicercaDocenteLineaBase,
+    RicercaLineaApplicata,
+    RicercaLineaBase,
+)
 
 class BaseResearchLinesSerializer(serializers.ModelSerializer):
     description = serializers.CharField(source="descrizione")
@@ -33,7 +38,7 @@ class BaseResearchLinesSerializer(serializers.ModelSerializer):
         
     
     class Meta:
-        model = RicercaGruppo
+        model = RicercaLineaBase
         fields = [
             "id",
             "description",
@@ -41,6 +46,44 @@ class BaseResearchLinesSerializer(serializers.ModelSerializer):
             "year",
             "erc2Id",
             "erc2Name",
+            "teachers",
+            "visible"
+        ]
+
+
+class AppliedResearchLinessSerializer(serializers.ModelSerializer):
+    description = serializers.CharField(source="descrizione")
+    results = serializers.CharField(source="descr_pubblicaz_prog_brevetto")
+    year = serializers.IntegerField(source="anno")
+    aster2Id = serializers.CharField(source="ricerca_aster2_id.ricerca_aster1_id", allow_null=True)
+    aster2Name = serializers.CharField(source="ricerca_aster2_id.descrizione", allow_null=True)
+    teachers = serializers.SerializerMethodField()
+    visible = serializers.BooleanField(source="visibile")
+
+    def get_teachers(self, obj):
+        return [
+            {
+                "id": encrypt(docente.personale.matricola),
+                "name": (
+                    f"{docente.personale.cognome} {docente.personale.nome}"
+                    + (f" {docente.personale.middle_name}" if docente.personale.middle_name else "")
+                ),
+                "departmentName": docente.personale.ds_sede,
+                "departmentCod": docente.personale.sede,
+            }
+            for docente in obj.ricercadocentelineaapplicata_set.all()
+        ]
+        
+    
+    class Meta:
+        model = RicercaLineaApplicata
+        fields = [
+            "id",
+            "description",
+            "results",
+            "year",
+            "aster2Id",
+            "aster2Name",
             "teachers",
             "visible"
         ]
--- research_lines/api/v2/views.py
+++ research_lines/api/v2/views.py
@@ -9,13 +9,16 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import mixins, viewsets
 
-from .filters import BaseResearchLinesFilter
+from .filters import (
+    BaseResearchLinesFilter,
+    AppliedResearchLinesFilter
+    )
 
 from .serializers import (
-    BaseResearchLinesSerializer
+    BaseResearchLinesSerializer,
+    AppliedResearchLinessSerializer
 )
 from django.db.models import Prefetch, Q
-from research_groups.models import RicercaDocenteGruppo
 from research_lines.models import (
     RicercaDocenteLineaApplicata,
     RicercaDocenteLineaBase,
@@ -24,7 +27,6 @@ from research_lines.models import (
 )
 from organizational_area.models import OrganizationalStructureOfficeEmployee
 from research_lines.settings import OFFICE_RESEARCH_LINES
-from teachers.api.v1.services import ServiceDocente
 
 
 class BaseResearchLinesViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -77,3 +79,56 @@ class BaseResearchLinesViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
             )
             .distinct()
         )
+
+
+class AppliedResearchLinesViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    pagination_class = PageNumberPagination
+    filter_backends = [DjangoFilterBackend]
+    serializer_class = AppliedResearchLinessSerializer
+    filterset_class = AppliedResearchLinesFilter
+
+    def get_queryset(self):
+        only_active = True
+        if self.request.user.is_superuser:
+            only_active = False  # pragma: no cover
+        if self.request.user.is_authenticated:  # pragma: no cover
+            my_offices = OrganizationalStructureOfficeEmployee.objects.filter(
+                employee=self.request.user,
+                office__name=OFFICE_RESEARCH_LINES,
+                office__is_active=True,
+                office__organizational_structure__is_active=True,
+            )
+            if my_offices.exists():
+                only_active = False
+                
+        query_is_active = Q(visibile=True) if only_active else Q()
+                
+        return (
+            RicercaLineaApplicata.objects.filter(query_is_active)
+            .order_by("descrizione")
+            .prefetch_related(
+                Prefetch(
+                    "ricercadocentelineaapplicata_set",
+                    queryset=RicercaDocenteLineaApplicata.objects.select_related("personale").only(
+                        "personale__matricola",
+                        "personale__cognome",
+                        "personale__nome",
+                        "personale__middle_name",
+                        "personale__sede",
+                        "personale__ds_sede",
+                    )
+                )
+            )
+            .select_related("ricerca_aster2")
+            .only(
+                "id",
+                "descrizione",
+                "descr_pubblicaz_prog_brevetto",
+                "anno",
+                "ricerca_aster2_id__ricerca_aster1_id",
+                "ricerca_aster2_id__descrizione",
+                "visibile",
+            )
+            .distinct()
+        )
+
