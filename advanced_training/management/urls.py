6e48a3979b3c91a34aa9164b559326b3944d1db5
revelis-martina <martina.imbrogno@revelis.eu>
2025-09-03 17:03:12 +0200

    fix: delete fix

--- advanced_training/management/urls.py
+++ advanced_training/management/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     advancedtraining_info_edit,
     advancedtraining_masters,
-    advancedtraining_info_create
+    advancedtraining_info_create,
+    advancedtraining_info_delete
 )
 
 app_name = "management"
@@ -15,4 +16,10 @@ urlpatterns = [
         name="advanced-training-detail",
     ),
     path("masters/new/", advancedtraining_info_create, name="advanced-training-create"),
+    path(
+    "advanced-training/<int:pk>/delete/",
+    advancedtraining_info_delete,
+    name="advanced-training-delete",
+),
+
 ]
