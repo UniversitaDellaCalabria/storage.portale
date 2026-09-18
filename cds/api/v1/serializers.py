f41856fdd3157b1c2ca521d8af31aad1d0be4e52
Francesco Filicetti <easyweb.wa@gmail.com>
2026-09-18 09:01:26 +0200

    fix:_ bug

--- cds/api/v1/serializers.py
+++ cds/api/v1/serializers.py
@@ -246,9 +246,9 @@ class CdsInfoSerializer(serializers.Serializer):
             # email_id_coordinatore = q["matricola_coordinatore__email"].split("@")[0] if q["matricola_coordinatore__email"] and q["matricola_coordinatore__email"].endswith(f"@{ADDRESSBOOK_FRIENDLY_URL_MAIN_EMAIL_DOMAIN}") else None
             # email_id_vice = q["matricola_vice_coordinatore__email"].split("@")[0] if q["matricola_vice_coordinatore__email"] and q["matricola_vice_coordinatore__email"].endswith(f"@{ADDRESSBOOK_FRIENDLY_URL_MAIN_EMAIL_DOMAIN}") else None
             return {
-                "DirectorId": str(q["matricola_coordinatore__id_ab"]),
+                "DirectorId": str(q["matricola_coordinatore__id_ab"]) if q["matricola_coordinatore__id_ab"] else None,
                 "DirectorName": q["nome_origine_coordinatore"],
-                "DeputyDirectorId": str(q["matricola_vice_coordinatore__id_ab"]),
+                "DeputyDirectorId": str(q["matricola_vice_coordinatore__id_ab"]) if q["matricola_vice_coordinatore__id_ab"] else None,
                 "DeputyDirectorName": q["nome_origine_vice_coordinatore"],
                 # 'SeatsNumber': q['num_posti'],
                 # 'RegistrationMode': q['modalita_iscrizione'],
@@ -267,7 +267,7 @@ class CdsInfoSerializer(serializers.Serializer):
                 {
                     "Order": q["ordine"],
                     "OfficeName": q["nome_ufficio"],
-                    "OfficeDirector": str(q["matricola_riferimento__id_ab"]),
+                    "OfficeDirector": str(q["matricola_riferimento__id_ab"]) if q["matricola_riferimento__id_ab"] else None,
                     "OfficeDirectorName": q["nome_origine_riferimento"],
                     "TelOffice": q["telefono"],
                     "Email": q["email"],
